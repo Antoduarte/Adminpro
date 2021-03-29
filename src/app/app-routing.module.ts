35e25107060701ac88744a08fcb2c797102999ab
Nestor Duarte <antoduarte192212@gmail.com>
2021-03-29 11:27:51 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+   component:PagesComponent,
+   children:[
+    {path:'graficas1', component:Graficas1Component},
+    {path:'progress', component:ProgressComponent},
+    {path:'dashboard', component:DashboardComponent},
+    {path:'', pathMatch:'full', redirectTo:'dashboard'},
+   ]
+  },
+ 
+  {path:'login', component:LoginComponent},
+  {path:'registro', component:RegisterComponent}, 
+  {path:'**', component:NopagefoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
